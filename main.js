5d1482aa5dfdbc28c4bd2bb896e22ab3cef5cc19
Sanya-0206-debug <82977139+Sanya-0206-debug@users.noreply.github.com>
2022-06-13 15:47:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ function draw(){
     background('#D3D3D3')
     fill('#ff0000');
     stroke('#000000');
-    square(noseX, noseY, difference);
-    document.getElementById('square_side').innerHTML = "Width and Height of the Square will be =" + difference+"px";
+    square(noseX, noseY, diffrence);
+    document.getElementById('square_side').innerHTML = "Width and Height of the Square will be =" + diffrence+"px";
 }
 function gotPoses(results){
     if(results.length > 0){
@@ -28,7 +28,7 @@ function gotPoses(results){
     noseY= results[0].pose.nose.y;
     leftWristX= results[0].pose.leftWrist.x;
     rightWristX= results[0].pose.rightWrist.x;
-    difference =  floor(leftWristX - rightWristX);
+    diffrence =  floor(leftWristX - rightWristX);
     }
 
 }
